693b021ec541ec8de60ce70cc22ba63a86adffc6
@Starlord0987 <shekharsahani0987@gmail.com>
2022-10-12 18:17:42 +0530

    first commit

---
+++ upcoming_anime.js
@@ -0,0 +1,316 @@
+var jsonData = [
+    {
+        "title": "Love Flops (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25472-1359357894.1648203036.jpg",
+        "fall": "2022-10-12"
+    },
+    {
+        "title": "Aru Asa Dummy Head Mike ni Natteita Ore-kun no Jinsei (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26073-960959725.1657711679.jpg",
+        "fall": "2022-10-12"
+    },
+    {
+        "title": "Urusei Yatsura (TV 2022)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25106-1201407355.1663207740.jpg",
+        "fall": "2022-10-13"
+    },
+    {
+        "title": "The Little Lies We All Tell (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25481-737699012.1662752666.jpg",
+        "fall": "2022-10-15"
+    },
+    {
+        "title": "Uma Yuru (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25655-1500093411.1651753722.jpg",
+        "fall": "2022-10-16"
+    },
+    {
+        "title": "To Your Eternity (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24694-1755567054.1662444511.jpg",
+        "fall": "2022-10-23"
+    },
+    {
+        "title": "Arknights: Prelude to Dawn (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24899-3498534896.1664017558.jpg",
+        "fall": "2022-10-28"
+    },
+    {
+        "title": "Kan Colle Season 2: Let's Meet at Sea (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25111-239553496.1648278953.jpg",
+        "fall": "2022-11-03"
+    },
+    {
+        "title": "Me & Roboco (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25734-2304864693.1664123638.jpg",
+        "fall": "2022-12-04"
+    },
+    {
+        "title": "Technoroid Overmind (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24952-1350899842.1647000258.jpg",
+        "fall": "2022"
+    },
+    {
+        "title": "Chainsaw Man (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A23843-1865191988.1659747504.jpg",
+        "fall": "2022-10-11"
+    },
+    {
+        "title": "Eikyū Shōnen Eternal Boys (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25445-1862708933.1660770771.jpg",
+        "fall": "2022-10-10"
+    },
+    {
+        "title": "Play It Cool, Guys (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25572-1416279854.1662548140.jpg",
+        "fall": "2022-10-10"
+    },
+    {
+        "title": "Bleach: Thousand-Year Blood War (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25066-825432658.1659822888.jpg",
+        "fall": "2022-10-10"
+    },
+    {
+        "title": "Peter Grill and the Philosopher's Time - Super Extra (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24966-1843961505.1650860301.jpg",
+        "fall": "2022-10-09"
+    },
+    {
+        "title": "More Than a Married Couple, But Not Lovers (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24989-3093803048.1659405899.jpg",
+        "fall": "2022-10-09"
+    },
+    {
+        "title": "BLUELOCK (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24623-1590350266.1628767885.jpg",
+        "fall": "2022-10-08"
+    },
+    {
+        "title": "Pui Pui Molcar Driving School (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25737-2178263561.1657779390.jpg",
+        "fall": "2022-10-08"
+    },
+    {
+        "title": "Welcome to Demon School, Iruma-kun (TV 3)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24714-3253122830.1660582387.jpg",
+        "fall": "2022-10-08"
+    },
+    {
+        "title": "Yowamushi Pedal Limit Break (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25039-2727195970.1638919817.jpg",
+        "fall": "2022-10-08"
+    },
+    {
+        "title": "Bocchi the Rock! (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24062-1744295529.1657502501.jpg",
+        "fall": "2022-10-08"
+    },
+    {
+        "title": "Reiwa no Di Gi Charat (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24451-1788263521.1656894888.jpg",
+        "fall": "2022-10-07"
+    },
+    {
+        "title": "Legend of Mana: The Teardrop Crystal (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24482-1696268539.1651003295.jpg",
+        "fall": "2022-10-07"
+    },
+    {
+        "title": "Akiba Maid War (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25911-2273390334.1656100100.jpg",
+        "fall": "2022-10-06"
+    },
+    {
+        "title": "Bibliophile Princess (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25243-1826024138.1660245903.jpg",
+        "fall": "2022-10-06"
+    },
+    {
+        "title": "Sylvanian Families: Flare no Happy Diary (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26293-990883498.1662532748.jpg",
+        "fall": "2022-10-06"
+    },
+    {
+        "title": "The Eminence in Shadow (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24085-2135932930.1663150644.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Mob Psycho 100 III (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24878-3180891715.1634676996.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "The Human Crazy University (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25654-2951513933.1662725512.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Futoku no Guild (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25426-2281867310.1662825035.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Do It Yourself!! (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24186-2097134182.1660856680.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Obake Zukan! (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26027-3030294545.1657093174.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Reincarnated as a Sword (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24757-2936090316.1648370195.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Muv-Luv Alternative (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25078-2341389966.1661324383.jpg",
+        "fall": "2022-10-05"
+    },
+    {
+        "title": "Shinobi no Ittoki (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25371-2352285955.1658476000.jpg",
+        "fall": "2022-10-04"
+    },
+    {
+        "title": "VazzRock the Animation (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25695-1291312252.1663612941.jpg",
+        "fall": "2022-10-04"
+    },
+    {
+        "title": "Encouragement of Climb: Next Summit (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24161-2477601477.1660207276.jpg",
+        "fall": "2022-10-04"
+    },
+    {
+        "title": "Management of a Novice Alchemist (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24745-2603337610.1649398493.jpg",
+        "fall": "2022-10-03"
+    },
+    {
+        "title": "Golden Kamuy (TV 4)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25032-1890086850.1638749915.jpg",
+        "fall": "2022-10-03"
+    },
+    {
+        "title": "Mamekichi Mameko NEET no Nichijō (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26333-2331937825.1662989199.jpg",
+        "fall": "2022-10-03"
+    },
+    {
+        "title": "Detective Conan: The Culprit Hanzawa (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24818-1949677289.1633341953.jpg",
+        "fall": "2022-10-03"
+    },
+    {
+        "title": "Housing Complex C (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25449-1181293329.1660676380.jpg",
+        "fall": "2022-10-02"
+    },
+    {
+        "title": "PuniRunes (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26253-3205113521.1661842223.jpg",
+        "fall": "2022-10-02"
+    },
+    {
+        "title": "Mobile Suit Gundam: The Witch From Mercury (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24724-1022533231.1662298609.jpg",
+        "fall": "2022-10-02"
+    },
+    {
+        "title": "Beast Tamer (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25786-2260032171.1660070220.jpg",
+        "fall": "2022-10-02"
+    },
+    {
+        "title": "IDOLiSH7 Third Beat! (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24799-2551222339.1658901706.jpg",
+        "fall": "2022-10-02"
+    },
+    {
+        "title": "Pop Team Epic (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25092-3264319039.1660241914.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "Berserk: The Golden Age Arc - Memorial Edition (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26373-2284828593.1664004178.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "Uzaki-chan Wants to Hang Out! (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24609-2518366280.1655975139.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "I've Somehow Gotten Stronger When I Improved My Farm-Related Skills (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25398-2485299339.1661324548.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "Spy×Family (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25920-1941029535.1661708483.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "My Hero Academia (TV 6)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24773-1205524557.1658671109.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "Raven of the Inner Palace (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A25053-3090459900.1661011140.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "I'm the Villainess, So I'm Taming the Final Boss (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24807-2357962900.1659065319.jpg",
+        "fall": "2022-10-01"
+    },
+    {
+        "title": "My Master Has No Tail (TV)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A24601-2132374572.1662666766.jpg",
+        "fall": "2022-09-30"
+    },
+    {
+        "title": "Chickip Dancers (TV 2)",
+        "image": "//cdn.animenewsnetwork.com/thumbnails/fit200x200/encyc/A26194-2103613272.1660011989.jpg",
+        "fall": "2022-09-27"
+    }
+]
+
+const myContent = document.getElementById('output')
+{/* <li class="author"><a href="#">user name</a></li> */}
+{/* <li class="tags">
+         <ul>
+           <li><a href="#">C++</a></li>
+           <li><a href="#">Python</a></li>
+           <li><a href="#">Java</a></li>
+           <li><a href="#">JavaScript</a></li>
+         </ul>
+       </li> */}
+const showInnerHtml = jsonData.map((item, index) => {
+   return `<div style="margin-bottom:30px;" class="${index % 2 === 0 ? 'blog-card ' : 'blog-card alt'} ">
+   <div class="meta">
+     <div class="photo" style="background-image: url(${item.image}); background-size: contain;"></div>
+     <ul class="details">
+       
+       <li class="date">${item.fall} </li>
+       
+     </ul>
+   </div>
+   <div  class="description">
+     <h1 >${item.title} </h1>
+     <h2>Lorem ipsum dolor sit amet</h2>
+     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+     
+   </div>
+ </div>`
+}).join('')
+
+myContent.innerHTML = showInnerHtml
+
+// var output = document.getElementById('outpu');
+// output.innerHTML = jsonData[0].title
